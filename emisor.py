f6197ea14485e1bf5b84f97e275762c595be9ac4
JavierCotto5201 <53280525+JavierCotto5201@users.noreply.github.com>
2022-07-28 21:18:00 -0600

    Update emisor.py

--- emisor.py
+++ emisor.py
@@ -1,14 +1,29 @@
-def enviar_cadena():
-    print("")
+from bitarray import bitarray
+import random
 
+class emisor(object):
+    def enviar_cadena(self):
+        msj = input("Ingrese mensaje a enviar: ")
+        self.enviar_cadena_segura(msj)
 
-def enviar_cadena_segura():
-    print("")
+    def enviar_cadena_segura(self, msj):
+        a = bitarray()
+        a.frombytes(msj.encode('utf-8'))
+        self.agregar_ruido(a)
 
+    def agregar_ruido(self, a):
+        err = 0.10 
+        prob = round(random.random(), 2)
 
-def agregar_ruido():
-    print("")
+        if prob == err:
+            pos = random.randint(0, len(a))
+            if a[pos] == 1:
+                a[pos] = 0
+            else:
+                a[pos] = 1
 
+    def enviar_objeto():
+        print("")
 
-def enviar_objeto():
-    print("")
+llamar = emisor()
+llamar.enviar_cadena_segura("hola")
