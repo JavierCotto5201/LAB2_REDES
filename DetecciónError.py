b467155b67215a182ebd0b589f88d28fcb47594f
JavierCotto5201 <53280525+JavierCotto5201@users.noreply.github.com>
2022-07-28 21:23:22 -0600

    encabezado

---
+++ DetecciónError.py
@@ -0,0 +1,10 @@
+#-----------------------------------#
+#|Universidad del Valle de Guatemala|
+#|----------------------------------|
+#| Redes - Sección 20 - 02/08/2022  |
+#|----------------------------------|
+#|Integrantes:                      |
+#| Jose Abraham Gutierrez Corado    |
+#| Walter Danilo Saldaña Salguero   |
+#| Javier Alejandro Cotto Argueta   |
+#-----------------------------------#
--- emisor.py
+++ emisor.py
@@ -1,3 +1,14 @@
+#-----------------------------------#
+#|Universidad del Valle de Guatemala|
+#|----------------------------------|
+#| Redes - Sección 20 - 02/08/2022  |
+#|----------------------------------|
+#|Integrantes:                      |
+#| Jose Abraham Gutierrez Corado    |
+#| Walter Danilo Saldaña Salguero   |
+#| Javier Alejandro Cotto Argueta   |
+#-----------------------------------#
+
 from bitarray import bitarray
 import random
 
